b31ce027af7fdd92316795602b530377c5de1293
Kha <khab9thd@gmail.com>
2022-08-16 06:27:34 +0000

    Edited after_install.sh

--- deployment_scripts/after_install.sh
+++ deployment_scripts/after_install.sh
@@ -2,6 +2,7 @@
 cd /home/ec2-user/AWSFCJ
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
+nvm install --lts
 npm install 16
 npm install express dotenv express-handlebars body-parser mysql
 npm install --save-dev nodemon
