c63d6257c802fe933bf04b666737e6a4398cf395
Kha <khab9thd@gmail.com>
2022-08-16 06:39:08 +0000

    Edited start_server.sh

--- deployment_scripts/start_server.sh
+++ deployment_scripts/start_server.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -x
-cd /home/ec2-user/AWSFCJ
+cd /home/ec2-user/AWSFCJ/AWS-FCJ-Management
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
 npm install 16
 npm install express dotenv express-handlebars body-parser mysql
 npm install --save-dev nodemon
 npm install pm2@latest -g
-pm2 start app.js -f
+pm2 start /app.js -f
